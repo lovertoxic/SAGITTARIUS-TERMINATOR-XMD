be8554919b19c35b22a49855e303fdf277ce11f6
JEEPERS CREEPER <lovertoxic91@gmail.com>
2025-07-19 00:03:10 +0300

    Update gen-pass.js

--- plugins/gen-pass.js
+++ plugins/gen-pass.js
@@ -56,7 +56,7 @@ cmd({
 
     // Send the message with the generated password
     await conn.sendMessage(from, {
-      text: "🔐 *Your Strong Password* 🔐\n\nPlease find your generated password below:\n\n" + generatedPassword + "\n\n*Powered By CRISS-AI*"
+      text: "🔐 *Your Strong Password* 🔐\n\nPlease find your generated password below:\n\n" + generatedPassword + "\n\n*Powered By sir bravin*"
     }, {
       quoted: quoted
     });
