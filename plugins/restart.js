05018bcce70e883a96120d8ea19813c8366f3691
JEEPERS CREEPER <lovertoxic91@gmail.com>
2025-07-18 21:56:46 +0300

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -3,7 +3,7 @@ const { sleep } = require("../lib/functions");
 
 cmd({  
     pattern: "restart",  
-    desc: "Restart CRISS-AI",  
+    desc: "Restart sir bravin",  
     category: "owner",  
     filename: __filename  
 },  
