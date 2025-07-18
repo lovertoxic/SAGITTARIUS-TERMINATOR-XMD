90247d08e78b958e89c03ad448c91a12c3c41a08
JEEPERS CREEPER <lovertoxic91@gmail.com>
2025-07-18 22:38:37 +0300

    Update anti-once.js

--- plugins/anti-once.js
+++ plugins/anti-once.js
@@ -2,7 +2,7 @@ const { cmd } = require("../command");
 
 cmd({
   pattern: "vv2",
-  alias: ["wah", "ohh", "oho", "🙂", "nice", "ok"],
+  alias: ["wah", "ohh", "oho", "🌬️", "nice", "ok"],
   desc: "Owner Only - retrieve quoted message back to user",
   category: "owner",
   filename: __filename
