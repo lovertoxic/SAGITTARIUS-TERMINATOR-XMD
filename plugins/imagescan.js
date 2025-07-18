b3e06ff676ca889c910722acd84a41fadc46be66
JEEPERS CREEPER <lovertoxic91@gmail.com>
2025-07-18 23:15:43 +0300

    Update imagescan.js

--- plugins/imagescan.js
+++ plugins/imagescan.js
@@ -75,7 +75,7 @@ cmd({
     await reply(
       `🔍 *Image Analysis Results*\n\n` +
       `${scanResponse.data.result}\n\n` +
-      `> © Powered by CRISS AI 💜`
+      `> © Powered by sir bravin 💜`
     );
 
   } catch (error) {
