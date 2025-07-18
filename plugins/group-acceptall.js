82d456bfb0997995c5d92166e89a2e9d020a5d33
JEEPERS CREEPER <lovertoxic91@gmail.com>
2025-07-18 23:49:17 +0300

    Update group-acceptall.js

--- plugins/group-acceptall.js
+++ plugins/group-acceptall.js
@@ -5,7 +5,7 @@ cmd({
     pattern: "requestlist",
     desc: "Shows pending group join requests",
     category: "group",
-    react: "📋",
+    react: "🌐",
     filename: __filename
 },
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
