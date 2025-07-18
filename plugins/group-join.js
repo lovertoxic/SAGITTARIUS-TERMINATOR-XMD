eebb1c26b249bd7c14ecbead2e67b47d48e603c6
JEEPERS CREEPER <lovertoxic91@gmail.com>
2025-07-18 23:43:34 +0300

    Update group-join.js

--- plugins/group-join.js
+++ plugins/group-join.js
@@ -4,7 +4,7 @@ const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, 
 
 cmd({
     pattern: "join",
-    react: "📬",
+    react: "🩱",
     alias: ["joinme", "f_join"],
     desc: "To Join a Group from Invite link",
     category: "group",
