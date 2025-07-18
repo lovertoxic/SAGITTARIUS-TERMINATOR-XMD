ccac140ef6a842941335fa4d1e63473dd412492e
JEEPERS CREEPER <lovertoxic91@gmail.com>
2025-07-18 21:41:51 +0300

    Update tool-converter.js

--- plugins/tool-converter.js
+++ plugins/tool-converter.js
@@ -35,7 +35,7 @@ cmd({
         // Send result
         await client.sendMessage(from, {
             image: imageBuffer,
-            caption: "> Powered By JawadTechX 🤍",
+            caption: "> Powered By  sir bravin",
             mimetype: 'image/png'
         }, { quoted: message });
 
