2b819b7ce30dd3941d91136e8e9f12ad25237a94
JEEPERS CREEPER <lovertoxic91@gmail.com>
2025-07-18 21:14:44 +0300

    Update tts-trt.js

--- plugins/tts-trt.js
+++ plugins/tts-trt.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, q, reply }) => {
         const response = await axios.get(url);
         const translation = response.data.responseData.translatedText;
 
-        const translationMessage = `> *CRISS-AI-TRANSLATION*
+        const translationMessage = `> *sir bravin-translation*
 
 > 🔤 *Original*: ${textToTranslate}
 
