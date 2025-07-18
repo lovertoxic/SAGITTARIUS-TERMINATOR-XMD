1d33bc04d650e2663fa6d259b5a1098de4ffcf1b
JEEPERS CREEPER <lovertoxic91@gmail.com>
2025-07-18 21:24:33 +0300

    Update tool-vfc.js

--- plugins/tool-vfc.js
+++ plugins/tool-vfc.js
@@ -39,8 +39,8 @@ cmd({
         await conn.sendMessage(from, {
             document: fs.readFileSync(nmfilect), 
             mimetype: 'text/vcard', 
-            fileName: 'ʜᴜɴᴛᴇʀ xᴍᴅ.vcf', 
-            caption: `\nDone saving.\nGroup Name: *${cmiggc.subject}*\nContacts: *${cmiggc.participants.length}*\n> ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʜᴜɴᴛᴇʀ xᴍᴅ`}, { quoted: mek });
+            fileName: 'sir bravin.vcf', 
+            caption: `\nDone saving.\nGroup Name: *${cmiggc.subject}*\nContacts: *${cmiggc.participants.length}*\n> ᴘᴏᴡᴇʀᴇᴅ ʙʏ sir bravin`}, { quoted: mek });
 
         fs.unlinkSync(nmfilect); // Cleanup the file after sending
     } catch (err) {
