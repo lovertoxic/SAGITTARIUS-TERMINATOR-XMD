d19e981302a99e136d3f0185967cce440a9a6a33
JEEPERS CREEPER <lovertoxic91@gmail.com>
2025-07-19 16:19:28 +0300

    Update config.js

--- config.js
+++ config.js
@@ -41,7 +41,7 @@ OWNER_NUMBER: process.env.OWNER_NUMBER || "254717263689",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "sir bravin",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© powered by sir t*",
+DESCRIPTION: process.env.DESCRIPTION || "*© powered by sir bravin*",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/m4zhif.jpg",
 // add img for alive msg
@@ -71,7 +71,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "254701082940",
+DEV: process.env.DEV || "254717263689",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
