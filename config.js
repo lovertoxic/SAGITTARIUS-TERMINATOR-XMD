92aa1c6aab8e0337ed32c5d997a8da3e4765b13a
sir bravin 🩸 <lovertoxic91@gmail.com>
2025-07-23 14:39:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -79,7 +79,5 @@ ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log",
 // change it to 'same' if you want to resend deleted message in same chat 
 AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
 // make it true for auto recoding 
-ANTICALL; process.env.ANTICALL || "true",
-//make it false to remove anticall 
 };
 };
