c22de759d7e788207cb054b13094116cedf44c97
sir bravin 🩸 <lovertoxic91@gmail.com>
2025-08-07 06:43:12 +0300

    Update config.js

--- config.js
+++ config.js
@@ -71,7 +71,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "254740694462",
+DEV: process.env.DEV || "255625606354",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
