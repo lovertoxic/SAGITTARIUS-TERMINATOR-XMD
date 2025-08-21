f7860adad257b08b23a8e7d7b939dbd049ee1e44
sir Arnold T20🩸 <lovertoxic91@gmail.com>
2025-08-21 11:53:06 +0300

    Update tool-url.js

--- plugins/tool-url.js
+++ plugins/tool-url.js
@@ -65,7 +65,7 @@ cmd({
       `*${mediaType} Uploaded Successfully*\n\n` +
       `*Size:* ${formatBytes(mediaBuffer.length)}\n` +
       `*URL:* ${mediaUrl}\n\n` +
-      `> © Uploaded by sir bravin 💜`
+      `> © Uploaded by Arnold T20 💜`
     );
 
   } catch (error) {
