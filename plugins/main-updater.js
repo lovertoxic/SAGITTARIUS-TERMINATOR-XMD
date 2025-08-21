4723a912b98745b11a39ff7d5f8ca67f38ab8cb8
sir Arnold T20🩸 <lovertoxic91@gmail.com>
2025-08-21 12:45:36 +0300

    Update main-updater.js

--- plugins/main-updater.js
+++ plugins/main-updater.js
@@ -16,7 +16,7 @@ cmd({
     if (!isOwner) return reply("This command is only for the bot owner.");
 
     try {
-        await reply("🔍 Checking for toxic lover-v2 xmd updates...");
+        await reply("🔍 Checking for 𝔗𝔈ℜ𝕄𝕀ℕ𝔸𝔗𝕆ℝ xmd updates...");
 
         // Fetch the latest commit hash from GitHub
         const { data: commitData } = await axios.get("https://api.github.com/repos/criss-vevo/CRISS-AI/commits/main");
@@ -26,10 +26,10 @@ cmd({
         const currentHash = await getCommitHash();
 
         if (latestCommitHash === currentHash) {
-            return reply("✅ Your toxic lover-v2 xmd bot is already up-to-date!");
+            return reply("✅ Your 𝔗𝔈ℜ𝕄𝕀ℕ𝔸𝔗𝕆ℝ xmd bot is already up-to-date!");
         }
 
-        await reply("🚀 Updating toxic lover-v2 xmd Bot...");
+        await reply("🚀 Updating 𝔗𝔈ℜ𝕄𝕀ℕ𝔸𝔗𝕆ℝ xmd Bot...");
 
         // Download the latest code
         const zipPath = path.join(__dirname, "latest.zip");
@@ -44,7 +44,7 @@ cmd({
 
         // Copy updated files, preserving config.js and app.json
         await reply("🔄 Replacing files...");
-        const sourcePath = path.join(extractPath, "TOXIC LOVER-V2 XMD-main");
+        const sourcePath = path.join(extractPath, "SAGITTARIUS TERMINATOR XMD-main");
         const destinationPath = path.join(__dirname, '..');
         copyFolderSync(sourcePath, destinationPath);
 
