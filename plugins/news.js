c7b8163e136a123ca89bfb72892b153deed84b1d
sir Arnold T20🩸 <lovertoxic91@gmail.com>
2025-08-21 12:04:09 +0300

    Update news.js

--- plugins/news.js
+++ plugins/news.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, reply }) => {
 ⚠️ _${article.description}_
 🔗 _${article.url}_
 
-  ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ sir bravin 
+  ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ₮₂₀☆𓂀𝕊𝕋𝔸ℝ𝔹𝕆𝕐𓂀
             `;
 
             console.log('Article URL:', article.urlToImage); // Log image URL for debugging
